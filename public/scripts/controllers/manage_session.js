a08b78990b24e2a4cbdefc3c52daddd7820b03d1
Harpreet Singh <singhh@tcd.ie>
2016-03-08 02:32:56 +0000

    Added controller for sessions

---
+++ public/scripts/controllers/manage_session.js
@@ -0,0 +1,12 @@
+
+module.exports = { name:"ManageSession", 
+
+controller:[function($scope){
+	$scope = {} 
+    $scope.states = ('Leinster Ulster Icantremember').split(' ').map(function(state) {
+        return {state};
+      })
+}]}
+
+
+	

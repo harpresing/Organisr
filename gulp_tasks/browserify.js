7ee3a91f9bb0d3ff48fd099ac553cf3e3c79959a
Stephen O'Kennedy <stephenoken@gmail.com>
2016-03-23 10:01:28 +0000

    "Uglified javascript"
    ""

--- gulp_tasks/browserify.js
+++ gulp_tasks/browserify.js
@@ -6,15 +6,17 @@ const browserify = require('browserify');
 const watchify = require('watchify');
 const babel = require('babelify');
 const browserSync = require("browser-sync");
-
+const uglify = require("gulp-uglify");
+const util = require('gulp-util');
 function compile(watch) {
-  var bundler = watchify(browserify('./public/scripts/app.js', { debug: true }).transform(babel));
+  var bundler = watchify(browserify('./public/scripts/app.js', { debug: true }).transform(babel,{presets: ["es2015"]}));
 
   function rebundle() {
     bundler.bundle()
       .on('error', function(err) { console.error(err); this.emit('end'); })
       .pipe(source('build.js'))
       .pipe(buffer())
+      // .pipe(ingore.exclude(["**/*.map"]))
       .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('./dist/scripts'));
@@ -36,13 +38,12 @@ function watch() {
 }
 
 gulp.task('prod-build-js',()=>{
-  var bundler = browserify('./public/scripts/app.js', { debug: true }).transform(babel);
+  var bundler = browserify('./public/scripts/app.js', { debug: true }).transform(babel,{presets: ["es2015"]});
   return bundler.bundle()
     .on('error', function(err) { console.error(err); this.emit('end'); })
     .pipe(source('build.js'))
     .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(sourcemaps.write('./'))
+    .pipe(uglify().on('error',util.log))
     .pipe(gulp.dest('./dist/scripts'));
 });
 gulp.task('build-js', function() { return compile(); });
